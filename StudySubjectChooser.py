f351659f8e6713c4f20aa3495a48e41e88f44e65
Fl0w3rCh1ld <90223489+Fl0w3rCh1ld@users.noreply.github.com>
2022-03-15 14:18:43 -0600

    Add files via upload

---
+++ StudySubjectChooser.py
@@ -0,0 +1,14 @@
+# Escoge la materia y el tema a estudiar
+import random
+
+ListaDeMateriasHumanidades = [
+    "Español",
+    "Literatura",
+    "Geografía",
+    "Historia de México",
+    "Historia Universal",
+    "Filosofía",
+]
+ListaDeMateriasCiencias = ["Biología", "Matemáticas", "Química", "Física"]
+print(random.choice(ListaDeMateriasHumanidades))
+print(random.choice(ListaDeMateriasCiencias))
---
+++ YesNoMaybe.py
@@ -0,0 +1,5 @@
+# YES/NO/MAYBE (TRY AGAIN)/
+import random
+#lista de prueba
+ListaDePrueba = ['Si','No','Tal vez (Intenta de nuevo)']
+print(random.choice(ListaDePrueba))
